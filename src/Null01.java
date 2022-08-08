398609c0eb147f7ef4aedc2fdaa65b9e2f6a5445
kosuke toyo <107592609+kosuke-toyo-1999@users.noreply.github.com>
2022-08-08 19:43:27 +0900

    JavaWcp

---
+++ src/Null01.java
@@ -0,0 +1,27 @@
+
+public class Null01 {
+	public static void main(String[] args) {
+	    long long1 = 10000000000L;
+	    System.out.println(long1);
+		
+		double double1 = 3.14;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Integer integer1 = 100;
+		System.out.println(integer1);
+		
+		float float2 = 1.4142f;
+		System.out.println(float2);
+		
+		char char1 = 'う';
+		System.out.println(char1);
+		
+		int[] array3 = new int[] { 0, 10, 20, 30, 40  };
+		System.out.println(array3[3]);
+	}
+
+
+}
